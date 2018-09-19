1839f63508e8fa8b10f4f22d9acef637a07c8120
KaytiSwift <kaytiswift@yahoo.com>
2018-09-19 11:55:16 -0400

    updated

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -10,7 +10,7 @@ namespace FizzBuzz
     {
         public string Get(int input)
         {
-            if (IsMultipleOf3(input) && IsMultipleOf5(input)) 
+            if (IsMultipleOf3And5(input)) 
             {
                 return "FizzBuzz";
             }
@@ -33,6 +33,11 @@ namespace FizzBuzz
         private bool IsMultipleOf5(int input)
         {
             return input % 5 == 0;
+        }
+
+        private bool IsMultipleOf3And5(int input)
+        {
+            return IsMultipleOf3(input) && IsMultipleOf5(input);
         }
     }
 }
